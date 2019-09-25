6ae0986fdb5803686280bafcff556b0bc8422969
Henrique Treza <trezah.it@outlook.com>
2019-09-25 11:30:10 -0300

    Create Exercicios_Diversos009.py

---
+++ Exercicios2/Exercicios_Diversos009.py
@@ -0,0 +1,192 @@
+# #Developed by Henrique Treza
+
+lista = {'Henrique', 'Fabiana', 'Maria Julia', 'Maria Fernanda'}
+nomes = str(input("insira um nome: "))
+for nome in lista:
+    nome == 'Henrique'
+    print("Nome mais bonito")
+    break
+else:
+    print("Outros nomes, são minha familia")
+
+
+
+print("O nome inserido foi {} e  Prémio é {} ".format(nomes, nome))
+
+
+print("##################################################### 1")
+
+
+frase = 'Curso em Video de Python'
+separado = frase.split()
+palavra = separado[2]
+letra = palavra[3]
+print(letra.upper())
+print("##################################################### 2")
+
+
+a = 4
+b = 3
+c = 2
+d = a + b * c
+e = d % c + 1
+print("{} e {}".format(d, e))
+print("##################################################### 3")
+
+
+from random import randint
+num = randint(1, 6)
+res = num // 2
+print(res)
+print("##################################################### 4")
+
+
+from random import choice
+n = [2, 5, 9, 1, 4]
+res = choice(n) % n[0]
+print(res)
+print("##################################################### 5")
+
+
+n1 = 5
+n2 = 2
+res = n1 / n2 // 1
+print(res)
+print("##################################################### 6")
+
+
+n1 = '7'
+
+n2 = 4
+
+print(n1 + str(n2))
+print("##################################################### 7")
+
+
+res = 5 * 3 ** 2
+
+print(res)
+print("##################################################### 8")
+
+from random import choice
+n = [2, 5, 9, 1, 4]
+res = choice(n) % n[0]
+print(res)
+print("##################################################### 9")
+
+
+frase = 'Curso em Video de Python'
+separado = frase.split()
+palavra = separado[2]
+letra = palavra[3]
+print(letra.upper())
+print("##################################################### 10")
+
+
+from random import randint
+num = randint(1, 6)
+res = num // 2
+print(res)
+print("##################################################### 11")
+
+
+num = 4.999
+
+print(int(num))
+print("##################################################### 12")
+
+
+num = '7'
+res = int(num) / 2
+print(type(res))
+print("##################################################### 13")
+
+
+nome = 'Henrique Treza Junqueira Rodrigues'
+print(nome[1:10:2].upper())
+print("##################################################### 14")
+
+
+texto = 'Tres Pratos de Trigo para Tigres Tristes'
+total = texto.upper().count(texto[0])
+print(total)
+print("##################################################### 15")
+
+
+from datetime import date
+ano = date.today().year
+print("##################################################### 16")
+
+
+from random import choice
+n = [2, 5, 9, 1, 4]
+res = choice(n) % n[0]
+print(res)
+print("##################################################### 17")
+
+
+print('''Curso em Vídeo''')
+print("##################################################### 18")
+
+
+frase = 'Curso em Video de Python'
+separado = frase.split()
+palavra = separado[2]
+letra = palavra[3]
+print(letra.upper())
+print("##################################################### 19")
+
+
+num = '7'
+res = int(num) / 2
+print(type(res))
+print("##################################################### 20")
+
+
+n1 = '7'
+
+n2 = 4
+
+print(n1 + str(n2))
+print("##################################################### 21")
+
+
+res = 3 + 2 * 4
+
+print(res)
+print("##################################################### 22")
+
+
+num = '7'
+res = int(num) / 2
+print(type(res))
+print("##################################################### 23")
+valor = '153'
+parte = "5"
+valor += parte
+print(valor.isnumeric())
+print("##################################################### 24")
+
+
+texto = 'Tres Pratos de Trigo para Tigres Tristes'
+total = texto.upper().count(texto[0])
+print(total)
+print("##################################################### 25")
+
+
+a = 4
+b = 3
+c = 2
+d = a + b * c
+e = d % c + 1
+print("{} e {}".format(d, e))
+print("##################################################### 26")
+
+
+from random import choice
+n = [2, 5, 9, 1, 4]
+res = choice(n) % n[0]
+print(res)
+print("##################################################### 27")
+
+
